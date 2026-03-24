f87d158dc42b2c5ab57dccbacdf19a1aefc189a2
Lukasz Filipiuk <lukaszfilipiuk@o2.pl>
2026-03-24 15:50:22 +0100

    + can edit book in details

--- biblioteca/Models/Book.cs
+++ biblioteca/Models/Book.cs
@@ -1,19 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace biblioteca.Models
 {
-    public class Book
+    public class Book : INotifyPropertyChanged
     {
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public int PublicationYear { get; set; }
-        public string Genre { get; set; }
-        public string Signature { get; set; }
-        public string Description { get; set; }
-        public bool IsAvailable { get; set; } = true;
+        private string _title;
+        private string _author;
+        private int _publicationYear;
+        private string _genre;
+        private string _signature;
+        private string _description;
+        private bool _isAvailable = true;
 
+        public string Title
+        {
+            get => _title;
+            set { _title = value; OnPropertyChanged(); }
+        }
+
+        public string Author
+        {
+            get => _author;
+            set { _author = value; OnPropertyChanged(); }
+        }
+
+        public int PublicationYear
+        {
+            get => _publicationYear;
+            set { _publicationYear = value; OnPropertyChanged(); }
+        }
+
+        public string Genre
+        {
+            get => _genre;
+            set { _genre = value; OnPropertyChanged(); }
+        }
+
+        public string Signature
+        {
+            get => _signature;
+            set { _signature = value; OnPropertyChanged(); }
+        }
+
+        public string Description
+        {
+            get => _description;
+            set { _description = value; OnPropertyChanged(); }
+        }
+
+        public bool IsAvailable
+        {
+            get => _isAvailable;
+            set { _isAvailable = value; OnPropertyChanged(); }
+        }
         public Book() { }
 
         public Book(string title, string author, int publicationYear, string genre, string signature, string description)
@@ -24,6 +67,38 @@ namespace biblioteca.Models
             Genre = genre;
             Signature = signature;
             Description = description;
+        }
+
+        public Book Clone()
+        {
+            return new Book
+            {
+                Title = Title,
+                Author = Author,
+                PublicationYear = PublicationYear,
+                Genre = Genre,
+                Signature = Signature,
+                Description = Description,
+                IsAvailable = IsAvailable
+            };
+        }
+
+        public void CopyFrom(Book other)
+        {
+            Title = other.Title;
+            Author = other.Author;
+            PublicationYear = other.PublicationYear;
+            Genre = other.Genre;
+            Signature = other.Signature;
+            Description = other.Description;
+            IsAvailable = other.IsAvailable;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
--- biblioteca/Views/BookDetails.xaml.cs
+++ biblioteca/Views/BookDetails.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using biblioteca.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -17,10 +18,125 @@ namespace biblioteca.Views
     /// </summary>
     public partial class BookDetails : Window
     {
+        private Boolean IsEditing = false;
+        private Book _originalBook;
+        private Book _editedBook;
         public BookDetails(Models.Book book)
         {
             InitializeComponent();
             DataContext = book;
+            _originalBook = book;
+
+            LoadViewValues();
+            EnterViewMode();
+        }
+
+        private void LoadViewValues()
+        {
+            TitleText.Text = _originalBook.Title;
+            AuthorText.Text = _originalBook.Author;
+            PublicationYearText.Text = _originalBook.PublicationYear.ToString();
+            GenreText.Text = _originalBook.Genre;
+            SignatureText.Text = _originalBook.Signature;
+            DescriptionText.Text = _originalBook.Description;
+            IsAvailableText.Text = _originalBook.IsAvailable ? "Yes" : "No";
+        }
+
+        private void LoadEditValues()
+        {
+            TitleBox.Text = _editedBook.Title;
+            AuthorBox.Text = _editedBook.Author;
+            PublicationYearBox.Text = _editedBook.PublicationYear.ToString();
+            GenreBox.Text = _editedBook.Genre;
+            SignatureBox.Text = _editedBook.Signature;
+            DescriptionBox.Text = _editedBook.Description;
+            IsAvailableBox.IsChecked = _editedBook.IsAvailable;
+        }
+
+        private void ReadEditValues()
+        {
+            _editedBook.Title = TitleBox.Text;
+            _editedBook.Author = AuthorBox.Text;
+            _editedBook.Genre = GenreBox.Text;
+            _editedBook.Signature = SignatureBox.Text;
+            _editedBook.Description = DescriptionBox.Text;
+            _editedBook.IsAvailable = IsAvailableBox.IsChecked == true;
+        }
+
+        private void EnterViewMode()
+        {
+            TitleText.Visibility = Visibility.Visible;
+            AuthorText.Visibility = Visibility.Visible;
+            PublicationYearText.Visibility = Visibility.Visible;
+            GenreText.Visibility = Visibility.Visible;
+            SignatureText.Visibility = Visibility.Visible;
+            DescriptionText.Visibility = Visibility.Visible;
+            IsAvailableText.Visibility = Visibility.Visible;
+
+            TitleBox.Visibility = Visibility.Collapsed;
+            AuthorBox.Visibility = Visibility.Collapsed;
+            PublicationYearBox.Visibility = Visibility.Collapsed;
+            GenreBox.Visibility = Visibility.Collapsed;
+            SignatureBox.Visibility = Visibility.Collapsed;
+            DescriptionBox.Visibility = Visibility.Collapsed;
+            IsAvailableBox.Visibility = Visibility.Collapsed;
+
+            EditButton.Visibility = Visibility.Visible;
+            SaveButton.Visibility = Visibility.Collapsed;
+            CancelButton.Visibility = Visibility.Collapsed;
+        }
+
+        private void EnterEditMode()
+        {
+            TitleText.Visibility = Visibility.Collapsed;
+            AuthorText.Visibility = Visibility.Collapsed;
+            PublicationYearText.Visibility = Visibility.Collapsed;
+            GenreText.Visibility = Visibility.Collapsed;
+            SignatureText.Visibility = Visibility.Collapsed;
+            DescriptionText.Visibility = Visibility.Collapsed;
+            IsAvailableText.Visibility = Visibility.Collapsed;
+
+            TitleBox.Visibility = Visibility.Visible;
+            AuthorBox.Visibility = Visibility.Visible;
+            PublicationYearBox.Visibility = Visibility.Visible;
+            GenreBox.Visibility = Visibility.Visible;
+            SignatureBox.Visibility = Visibility.Visible;
+            DescriptionBox.Visibility = Visibility.Visible;
+            IsAvailableBox.Visibility = Visibility.Visible;
+
+            EditButton.Visibility = Visibility.Collapsed;
+            SaveButton.Visibility = Visibility.Visible;
+            CancelButton.Visibility = Visibility.Visible;
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            _editedBook = _originalBook.Clone();
+            LoadEditValues();
+            EnterEditMode();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(PublicationYearBox.Text, out int year))
+            {
+                MessageBox.Show("Publication Year must be a number.");
+                return;
+            }
+
+            ReadEditValues();
+            _editedBook.PublicationYear = year;
+
+            _originalBook.CopyFrom(_editedBook);
+            LoadViewValues();
+            EnterViewMode();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            _editedBook = _originalBook.Clone();
+            LoadViewValues();
+            EnterViewMode();
         }
     }
 }
