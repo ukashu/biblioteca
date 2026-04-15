69503d7e6b9d5640eeff0a74b2c91535b3fa559b
wi116659 <90299@student.pb.edu.pl>
2026-04-15 23:08:47 +0200

    adding Return Book panel

--- biblioteca/ViewModels/ReturnBooksViewModel.cs
+++ biblioteca/ViewModels/ReturnBooksViewModel.cs
@@ -1,11 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using biblioteca.MVVM;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
 
 namespace biblioteca.ViewModels
 {
-    public class ReturnBooksViewModel
+    public class ReturnBooksViewModel : INotifyPropertyChanged
     {
+        private string _inputValue;
+
         public string Title => "Return Books";
+
+        public string InputValue
+        {
+            get => _inputValue;
+            set
+            {
+                _inputValue = value;
+                OnPropertyChanged(nameof(InputValue));
+            }
+        }
+
+        public ObservableCollection<string> ReturnedBooks { get; set; }
+
+        public RelayCommand AddBookCommand => new RelayCommand(
+            execute => AddBook(),
+            canExecute => !string.IsNullOrWhiteSpace(InputValue)
+        );
+
+        public RelayCommand ReturnBooksCommand => new RelayCommand(
+            execute => ReturnBooks()
+        );
+
+        public ReturnBooksViewModel()
+        {
+            ReturnedBooks = new ObservableCollection<string>();
+        }
+
+        private void AddBook()
+        {
+            ReturnedBooks.Add(InputValue);
+            InputValue = string.Empty;
+        }
+
+        private void ReturnBooks()
+        {
+            MessageBox.Show("Zwrócono książki");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
