059a50a2891e3bc6b50cae3a270ca53712b5818f
Lukasz Filipiuk <lukaszfilipiuk@o2.pl>
2026-04-21 11:31:58 +0200

    + entity framework sqlite and proxies +DbContext

---
+++ biblioteca/Data/LibraryContext.cs
@@ -0,0 +1,21 @@
+﻿using biblioteca.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace biblioteca.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Loan> Loans { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlite("Data Source=library.db");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
