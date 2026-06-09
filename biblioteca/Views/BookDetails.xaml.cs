c7921c834b71539946cb336d6306ffa4e0cf4efa
Lukasz Filipiuk <lukaszfilipiuk@o2.pl>
2026-06-09 15:27:05 +0200

    +BookDetails Visibility fix

--- biblioteca/Views/BookDetails.xaml.cs
+++ biblioteca/Views/BookDetails.xaml.cs
@@ -56,48 +56,14 @@ namespace biblioteca.Views
 
         private void EnterViewMode()
         {
-            TitleText.Visibility = Visibility.Visible;
-            AuthorText.Visibility = Visibility.Visible;
-            PublicationYearText.Visibility = Visibility.Visible;
-            GenreText.Visibility = Visibility.Visible;
-            SignatureText.Visibility = Visibility.Visible;
-            DescriptionText.Visibility = Visibility.Visible;
-            IsAvailableText.Visibility = Visibility.Visible;
-
-            TitleBox.Visibility = Visibility.Collapsed;
-            AuthorBox.Visibility = Visibility.Collapsed;
-            PublicationYearBox.Visibility = Visibility.Collapsed;
-            GenreBox.Visibility = Visibility.Collapsed;
-            SignatureBox.Visibility = Visibility.Collapsed;
-            DescriptionBox.Visibility = Visibility.Collapsed;
-            IsAvailableBox.Visibility = Visibility.Collapsed;
-
-            EditButton.Visibility = Visibility.Visible;
-            SaveButton.Visibility = Visibility.Collapsed;
-            CancelButton.Visibility = Visibility.Collapsed;
+            ViewDetailsPanel.Visibility = Visibility.Visible;
+            EditDetailsPanel.Visibility = Visibility.Collapsed;
         }
 
         private void EnterEditMode()
         {
-            TitleText.Visibility = Visibility.Collapsed;
-            AuthorText.Visibility = Visibility.Collapsed;
-            PublicationYearText.Visibility = Visibility.Collapsed;
-            GenreText.Visibility = Visibility.Collapsed;
-            SignatureText.Visibility = Visibility.Collapsed;
-            DescriptionText.Visibility = Visibility.Collapsed;
-            IsAvailableText.Visibility = Visibility.Collapsed;
-
-            TitleBox.Visibility = Visibility.Visible;
-            AuthorBox.Visibility = Visibility.Visible;
-            PublicationYearBox.Visibility = Visibility.Visible;
-            GenreBox.Visibility = Visibility.Visible;
-            SignatureBox.Visibility = Visibility.Visible;
-            DescriptionBox.Visibility = Visibility.Visible;
-            IsAvailableBox.Visibility = Visibility.Visible;
-
-            EditButton.Visibility = Visibility.Collapsed;
-            SaveButton.Visibility = Visibility.Visible;
-            CancelButton.Visibility = Visibility.Visible;
+            ViewDetailsPanel.Visibility = Visibility.Collapsed;
+            EditDetailsPanel.Visibility = Visibility.Visible;
         }
 
         private void EditButton_Click(object sender, RoutedEventArgs e)
